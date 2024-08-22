653c7c7069d8935534952a832a131b77c16b545d
Jesse Peters <jjpeters@users.campus>
2024-08-22 16:24:35 -0500

    Added moveDown() function into the Field class for testing purposes.

--- main.js
+++ main.js
@@ -11,10 +11,22 @@ const pathCharacter = '*';
 class Field {
   constructor(field) {
     this._field = field;
+    this._cB = 0;
+    this._cS = 0;
+    this._location = field[this._cB][this._cS]
   };
   get field() {
     return this._field;
   };
+  get currentLocation() {
+    return this._location;
+  }
+  moveDown() {
+    this._cB =+ this._cB + 1;
+    this._field[this._cB][this._cS] = pathCharacter;
+    this._location = this._field[this._cB][this._cS]
+    this.print();
+  }
   print() {
     let currentField = fieldCreators.updateField(this._field);
     console.log(currentField);
@@ -33,6 +45,12 @@ let userChoiceField = Field.generateField(parseInt(userGridHeight), parseInt(use
 
 let firstGrid = new Field(userChoiceField);
 console.log(firstGrid)
+console.log(firstGrid.currentLocation)
 console.log(firstGrid.print())
-//console.log(firstGrid.field)
-//firstGrid.print();
+firstGrid.moveDown()
+console.log(firstGrid)
+
+
+
+
+
